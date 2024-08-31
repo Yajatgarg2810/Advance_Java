6b4363a401bf1143500caf3d15a8f09444797c46
Yajat Garg <145528836+Yajatgarg2810@users.noreply.github.com>
2024-08-31 10:22:42 +0530

    Add files via upload

---
+++ gui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+class gui{
+     public static void main(String args[]){
+            JFrame frame=new JFrame("My First GUI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300,300);
+            JButton button= new JButton("Press");
+            frame.add(button);
+            frame.setVisible(true);
+         }
+        }
